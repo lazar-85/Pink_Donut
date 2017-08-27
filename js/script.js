631669ce02eb5921c05f22b806bcb472f545b01e
Olexandr Lazarenko <lazarenkoolexandr@gmail.com>
2017-08-28 00:45:04 +0300

    28

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	  $('.main-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  
+  	  });
+
+
+  	  
+
+      
+});
+	
